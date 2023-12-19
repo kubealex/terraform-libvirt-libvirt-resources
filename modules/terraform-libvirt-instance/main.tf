e1d1c59c09d3af6d0febb40e4f660b7d207ceb7d
kubealex <al.rossi87@gmail.com>
2023-12-19 10:29:05 +0100

    fix firmware

--- modules/terraform-libvirt-instance/main.tf
+++ modules/terraform-libvirt-instance/main.tf
@@ -50,7 +50,7 @@ resource "libvirt_domain" "service-vm" {
   name = var.instance_count > 1 ? "${var.instance_hostname}-${count.index}" : var.instance_hostname
   memory = var.instance_memory*1024
   vcpu = var.instance_cpu
-  machine = var.instance_uefi_enabled ? "q35" : "pc-i440fx"
+  machine = var.instance_uefi_enabled ? "q35" : ""
   firmware = var.instance_uefi_enabled ? "/usr/share/edk2/ovmf/OVMF_CODE.fd" : ""
 
   boot_device {
